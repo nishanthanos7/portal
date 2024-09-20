0e876ee8721df5ec2be96e5a50b643c4ce2745a1
Nishan <nishan.poudel12345@gmail.com>
2024-09-20 11:51:11 +0545

    error message

--- app/util/db.ts
+++ app/util/db.ts
@@ -1,107 +1,107 @@
-export const users = [
-  {
-    "firstName": "ss",
-    "lastName": "mm",
-    "email": "mm",
-    "password": "mmm"
-  },
-  {
-    "firstName": "ss",
-    "lastName": "mm",
-    "email": "vvvv",
-    "password": "mmm"
-  },
-  {
-    "firstName": "ss",
-    "lastName": "mm",
-    "email": "vvvv",
-    "password": "mmm"
-  },
-  {
-    "firstName": "ss",
-    "lastName": "ss",
-    "email": "ss",
-    "password": "aa"
-  },
-  {
-    "firstName": "ss",
-    "lastName": "ss",
-    "email": "ss",
-    "password": "aa"
-  },
-  {
-    "firstName": "nischal",
-    "lastName": "bikas",
-    "email": "adma@gmail.com",
-    "password": "admam11"
-  },
-  {
-    "firstName": "ananta ",
-    "lastName": "bipal",
-    "email": "ananta@gmail.com",
-    "password": "ananta111"
-  },
-  {
-    "firstName": "ananta ",
-    "lastName": "bipal",
-    "email": "",
-    "password": "ananta111"
-  },
-  {
-    "firstName": "nis",
-    "lastName": "pod",
-    "email": "aaa@aaa",
-    "password": "ananta111"
-  },
-  {
-    "firstName": "nis",
-    "lastName": "pod",
-    "email": "aaa@aaa",
-    "password": "ananta111"
-  },
-  {
-    "firstName": "john ",
-    "lastName": "doe",
-    "email": "john@gmail.com",
-    "password": "john1"
-  },
-  {
-    "firstName": "Nishan",
-    "lastName": "Poudel",
-    "email": "nishan@gmail.com",
-    "password": "nishan1"
-  },
-  {
-    "firstName": "Bishal",
-    "lastName": "Gautaum",
-    "email": "bishal@gmail.com",
-    "password": "bishal12"
-  },
-  {
-    "firstName": "kaam",
-    "lastName": "kaam",
-    "email": "kaam",
-    "password": "kaam"
-  },
-  {
-    "firstName": "jack",
-    "lastName": "dorsy",
-    "email": "jack@g.com",
-    "password": "jack11"
-  },
-  {
-    "firstName": "jacky",
-    "lastName": "mine",
-    "email": "jack@g.com",
-    "password": "jackeee"
-  },
-  {
-    "firstName": "ishan",
-    "lastName": "tiwari",
-    "email": "ishan@gmail.com",
-    "password": "1111"
-  }
-];
+// export const users = [
+//   {
+//     "firstName": "ss",
+//     "lastName": "mm",
+//     "email": "mm",
+//     "password": "mmm"
+//   },
+//   {
+//     "firstName": "ss",
+//     "lastName": "mm",
+//     "email": "vvvv",
+//     "password": "mmm"
+//   },
+//   {
+//     "firstName": "ss",
+//     "lastName": "mm",
+//     "email": "vvvv",
+//     "password": "mmm"
+//   },
+//   {
+//     "firstName": "ss",
+//     "lastName": "ss",
+//     "email": "ss",
+//     "password": "aa"
+//   },
+//   {
+//     "firstName": "ss",
+//     "lastName": "ss",
+//     "email": "ss",
+//     "password": "aa"
+//   },
+//   {
+//     "firstName": "nischal",
+//     "lastName": "bikas",
+//     "email": "adma@gmail.com",
+//     "password": "admam11"
+//   },
+//   {
+//     "firstName": "ananta ",
+//     "lastName": "bipal",
+//     "email": "ananta@gmail.com",
+//     "password": "ananta111"
+//   },
+//   {
+//     "firstName": "ananta ",
+//     "lastName": "bipal",
+//     "email": "",
+//     "password": "ananta111"
+//   },
+//   {
+//     "firstName": "nis",
+//     "lastName": "pod",
+//     "email": "aaa@aaa",
+//     "password": "ananta111"
+//   },
+//   {
+//     "firstName": "nis",
+//     "lastName": "pod",
+//     "email": "aaa@aaa",
+//     "password": "ananta111"
+//   },
+//   {
+//     "firstName": "john ",
+//     "lastName": "doe",
+//     "email": "john@gmail.com",
+//     "password": "john1"
+//   },
+//   {
+//     "firstName": "Nishan",
+//     "lastName": "Poudel",
+//     "email": "nishan@gmail.com",
+//     "password": "nishan1"
+//   },
+//   {
+//     "firstName": "Bishal",
+//     "lastName": "Gautaum",
+//     "email": "bishal@gmail.com",
+//     "password": "bishal12"
+//   },
+//   {
+//     "firstName": "kaam",
+//     "lastName": "kaam",
+//     "email": "kaam",
+//     "password": "kaam"
+//   },
+//   {
+//     "firstName": "jack",
+//     "lastName": "dorsy",
+//     "email": "jack@g.com",
+//     "password": "jack11"
+//   },
+//   {
+//     "firstName": "jacky",
+//     "lastName": "mine",
+//     "email": "jack@g.com",
+//     "password": "jackeee"
+//   },
+//   {
+//     "firstName": "ishan",
+//     "lastName": "tiwari",
+//     "email": "ishan@gmail.com",
+//     "password": "1111"
+//   }
+// ];
 
 
 
@@ -110,4 +110,4 @@ export const users = [
 
 
 
-https://admin.ideaforworld.com/api/our-involvements
+// https://admin.ideaforworld.com/api/our-involvements
